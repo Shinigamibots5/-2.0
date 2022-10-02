ce16f398e34d5747884a9f2dfaabd61ee47f6ef2
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2022-10-02 19:03:40 +0530

    Update console.py

--- panipuri/console.py
+++ panipuri/console.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-from AdityaHalder.utilities.config import LOG_FILE_NAME
+from panipuri.utilities.config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
